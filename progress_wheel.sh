272af60d18637d8b0a754258cc0630a644fcf4e8
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2013-01-02 03:52:35 +0900

    add progress wheel

---
+++ progress_wheel.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo
+
+while [ 1 ]; do
+	echo -n ""
+	echo -n "\\"
+	sleep 1
+
+	echo -n ""
+	echo -n "|"
+	sleep 1
+
+	echo -n ""
+	echo -n "/"
+	sleep 1
+
+	echo -n ""
+	echo -n "|"
+	sleep 1
+done
+
