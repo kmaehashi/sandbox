4d2326c64b8f0069d0f8622d60e919e9f46ed59c
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2013-01-02 03:51:40 +0900

    add Java case sensitivity test

---
+++ java-class-case/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Test class");
+		new TEST();
+	}
+}
+
+class TEST {
+	public TEST() {
+		System.out.println("TEST class");
+	}
+}
+
