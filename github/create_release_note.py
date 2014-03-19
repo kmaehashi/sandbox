b1c726a2c36e6e661f184a341aaea3e78f6bda53
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2014-03-19 10:33:16 +0900

    release note creator

---
+++ github/create_release_note.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import urllib2
+import json
+
+# configuration
+owner = 'jubatus'
+repo = 'jubatus'
+state = 'closed'
+milestone = sys.argv[1] # must be a milestone ID (e.g., "21" for 0.5.3)
+
+def fetch(page):
+    # fetch from API (no authentication required for public repos)
+    url = 'https://api.github.com/repos/%s/%s/issues?milestone=%s&state=%s&page=%d' % (owner, repo, milestone, state, page)
+    response = urllib2.urlopen(url)
+
+    # parse and print as CSV
+    return json.loads(response.read())
+
+def do_print(issues):
+    for i in issues:
+        issue_id = str(i['number'])
+        milestone = i['milestone']
+        if milestone:
+            milestone_title = milestone['title']
+        else:
+            milestone_title = ''
+        created_user = i['user']['login']
+        title = i['title']
+        labels = i.get('labels', None)
+        if labels:
+            labels_list = '|'.join(map(lambda x: x['name'], labels))
+        else:
+            labels_list = ''
+        print '"%s","%s","%s","%s","%s"' % (issue_id, title, created_user, labels_list, milestone_title)
+
+for i in range(1,10):
+    issues = fetch(i)
+    if len(issues) == 0:
+        break
+    do_print(issues)
+
