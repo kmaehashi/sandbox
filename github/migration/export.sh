79a90c838101510938cc83f2e5b82c55644c5ae0
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2025-03-21 13:20:44 +0000

    update export script

--- github/migration/export.sh
+++ github/migration/export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Assumes GH_HOST (if GHES) and GH_TOKEN (admin:org, repo).
+# Assumes GH_TOKEN (admin:org, repo) OR GH_HOST & GH_ENTERPRISE_TOKEN if GHES.
 
 set -uex
 
@@ -8,15 +8,18 @@ src_org=$1
 src_repo=$2
 
 # Git data
-cat << _EOF_ | gh api '/orgs/${src_org}/migrations' --input -
+cat << _EOF_ | gh api "/orgs/${src_org}/migrations" --input -
 {"repositories":["${src_repo}"],"exclude_metadata":true}
 _EOF_
 
 # Metadata
-cat << _EOF_ | gh api '/orgs/${src_org}/migrations' --input -
+cat << _EOF_ | gh api "/orgs/${src_org}/migrations" --input -
 {"repositories":["${src_repo}"],"exclude_git_data":true,"exclude_releases":false,"lock_repositories":false,"exclude_owner_projects":true}
 _EOF_
 
-# Wait, then download
-# gh api /orgs/${src_org}/migrations/${MIGRATION_GIT_ID}/archive > git_archive.tar.gz
-# gh api /orgs/${src_org}/migrations/${MIGRATION_META_ID}/archive > meta_archive.tar.gz
+cat << _EOF_
+Wait, then download:
+
+# gh api /orgs/${src_org}/migrations/\${MIGRATION_GIT_ID}/archive > git_archive.tar.gz
+# gh api /orgs/${src_org}/migrations/\${MIGRATION_META_ID}/archive > meta_archive.tar.gz
+_EOF_
